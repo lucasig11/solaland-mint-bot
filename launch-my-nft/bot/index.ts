04b7e5f2f202a687418f774087db69e3ff3aaa5c
lucas <lucasfonsecard@icloud.com>
2022-11-25 19:56:02 -0300

    feat: get candy machine address from hyperspace url

--- launch-my-nft/bot/index.ts
+++ launch-my-nft/bot/index.ts
@@ -4,87 +4,68 @@ import {
   PublicKey,
   sendAndConfirmTransaction,
   Transaction,
-  TransactionInstruction,
 } from "@solana/web3.js";
 import { resolve } from "path";
 import { readFileSync } from "fs";
-
 import { LaunchMyNftCmClient } from "../client";
-import {
-  decodeCandyMachine,
-  // getCandyMachines,
-  getCandyMachineVersion,
-} from "../client/utils";
+import { getVersionedCandyMachine } from "../client/utils";
+import { fromTxError } from "../client/gen/errors";
 
 // Path to the payer's keypair file.
 const PAYER_KEYPAIR_FILE = resolve(__dirname, "..", "..", "keypair.json");
-
+// Hyperspace collection URL.
+const COLLECTION_URL =
+  "https://hyperspace.xyz/collection/Fqr9tGtG7rXG3Q9hWWZeyDG5o5d6UFqzjYFNaBfSEBF4";
 // Launch my NFT fee wallet (should be the same for every mint/candy machine).
-const LMN_FEE_WALLET = new PublicKey(
-  "33nQCgievSd3jJLSWFBefH3BJRN7h6sAoS82VFFdJGF5"
-);
-
-// Solana JsonRPC URL.
-const RPC_URL = "https://ssc-dao.genesysgo.net";
-
-// TODO: remove this constant once we can find the owner's candy machines.
-const CANDY_MACHINE_ADDRESS = new PublicKey(
-  "Fqr9tGtG7rXG3Q9hWWZeyDG5o5d6UFqzjYFNaBfSEBF4"
-);
-
-// TODO: use this constant instead of CANDY_MACHINE_ADDRESS
-// Collection creator (can be found through the collection page URL at LaunchMyNFT website).
-// const COLLECTION_CREATOR = "EZNvQ7aLyWXCiyHkLb33zcQvA6aefCFqaRzS4W8Ksk7o";
+const LMN_FEE_WALLET = "33nQCgievSd3jJLSWFBefH3BJRN7h6sAoS82VFFdJGF5";
+// Solana JSON RPC URL.
+const RPC_URL =
+  "https://solana-mainnet.g.alchemy.com/v2/bB-1A4zRy6e8n3OQcLsd1tk8Ni2KBwKr";
 
 (async () => {
   const connection = new Connection(RPC_URL);
-  const { createMintV5Instruction, createMintV6Instruction } =
-    LaunchMyNftCmClient(connection);
-
+  const feeWallet = new PublicKey(LMN_FEE_WALLET);
+  const mint = Keypair.generate();
+  const client = LaunchMyNftCmClient(connection);
   const payer = Keypair.fromSecretKey(
     Uint8Array.from(JSON.parse(readFileSync(PAYER_KEYPAIR_FILE, "utf8")))
   );
 
-  // TODO: find collection candy machine instead of using a static constant.
-  // const candyMachines = await getCandyMachines(
-  //   connection,
-  //   new PublicKey(COLLECTION_CREATOR)
-  // );
-  // console.log(
-  //   "Owner candy machines:",
-  //   candyMachines.map((c) => c.toJSON())
-  // );
+  const address = COLLECTION_URL.split("/").pop();
+  if (!address) throw new Error("Invalid collection URL");
 
-  const accInfo = await connection.getAccountInfo(CANDY_MACHINE_ADDRESS);
-  if (!accInfo) throw new Error("Account not found!");
+  const candyMachineAddress = new PublicKey(address);
+  const candyMachine = await getVersionedCandyMachine(
+    connection,
+    candyMachineAddress
+  );
+  if (!candyMachine) throw new Error("Candy machine not found!");
 
-  const candyMachineVersion = getCandyMachineVersion(accInfo.data);
-  const candyMachine = decodeCandyMachine(accInfo.data);
-  console.log(candyMachine.toJSON());
+  if (
+    candyMachine.version === "V2" ||
+    candyMachine.version === "V3" ||
+    candyMachine.version === "V4"
+  )
+    throw new Error("Unsupported candy machine version!");
 
-  const mint = Keypair.generate();
-  let ix: TransactionInstruction;
+  const ix = client[`createMint${candyMachine.version}Instruction`]({
+    feeWallet,
+    mint: mint.publicKey,
+    payer: payer.publicKey,
+    wallet: candyMachine.wallet,
+    candyMachine: candyMachineAddress,
+  });
 
-  switch (candyMachineVersion) {
-    case "v5":
-      ix = createMintV5Instruction({
-        mint: mint.publicKey,
-        payer: payer.publicKey,
-        wallet: candyMachine.wallet,
-        feeWallet: LMN_FEE_WALLET,
-        candyMachine: CANDY_MACHINE_ADDRESS,
-      });
-    case "v6":
-      ix = createMintV6Instruction({
-        mint: mint.publicKey,
-        payer: payer.publicKey,
-        wallet: candyMachine.wallet,
-        feeWallet: LMN_FEE_WALLET,
-        candyMachine: CANDY_MACHINE_ADDRESS,
-      });
+  try {
+    const tx = new Transaction().add(ix);
+    const txSig = await sendAndConfirmTransaction(connection, tx, [
+      payer,
+      mint,
+    ]);
+    console.log("Transaction signature:", txSig);
+  } catch (e) {
+    const parsed = fromTxError(e);
+    if (parsed) throw parsed;
+    throw e;
   }
-
-  const tx = new Transaction().add(ix);
-  const txSig = await sendAndConfirmTransaction(connection, tx, [payer, mint]);
-  console.log(txSig);
 })().then(() => console.log("done!"));
--- launch-my-nft/client/utils.ts
+++ launch-my-nft/client/utils.ts
@@ -1,60 +1,62 @@
-import { bs58 } from "@project-serum/anchor/dist/cjs/utils/bytes";
-import {
-  Connection,
-  GetProgramAccountsFilter,
-  MemcmpFilter,
-  PublicKey,
-} from "@solana/web3.js";
+import { Connection, PublicKey } from "@solana/web3.js";
 import axios from "axios";
-import { CandyMachineV5, CandyMachineV6 } from "./gen/accounts";
-import { PROGRAM_ID } from "./gen/programId";
+import {
+  CandyMachineV2,
+  CandyMachineV3,
+  CandyMachineV4,
+  CandyMachineV5,
+  CandyMachineV6,
+} from "./gen/accounts";
+import * as accounts from "./gen/accounts";
 
-export const getCandyMachines = async (
-  connection: Connection,
-  authority: PublicKey
-) => {
-  const cmV5Accounts = await fetchAccounts(connection, [
-    accountFilter(CandyMachineV5.discriminator),
-    memcmp(41, authority.toBase58()),
-  ]);
-
-  const cmV6Accounts = await fetchAccounts(connection, [
-    accountFilter(CandyMachineV5.discriminator),
-    memcmp(41, authority.toBase58()),
-  ]);
-
-  return cmV5Accounts
-    .concat(cmV6Accounts)
-    .map(({ account, pubkey }) =>
-      Object.assign(decodeCandyMachine(account.data), { pubkey })
-    );
+export type CandyMachineVersion = `V${2 | 3 | 4 | 5 | 6}`;
+export type CandyMachine =
+  | CandyMachineV2
+  | CandyMachineV3
+  | CandyMachineV4
+  | CandyMachineV5
+  | CandyMachineV6;
+export type VersionedCandyMachine = CandyMachine & {
+  version: CandyMachineVersion;
 };
 
-export const decodeCandyMachine = (data: Buffer): CandyMachine =>
-  lookup(data).machine.decode(data);
+export const getVersionedCandyMachine = async (
+  connection: Connection,
+  candyMachine: PublicKey
+): Promise<VersionedCandyMachine | null> => {
+  const account = await connection.getAccountInfo(candyMachine);
+  if (!account) return null;
+  return Object.assign(decodeCandyMachine(account.data), {
+    version: getCandyMachineVersion(account.data),
+  });
+};
 
-export const getCandyMachineVersion = (data: Buffer): CandyMachineVersion =>
-  lookup(data).version;
+export const decodeCandyMachine = (
+  data: Buffer,
+  version?: CandyMachineVersion
+): CandyMachine => {
+  return accounts[
+    `CandyMachine${version ?? getCandyMachineVersion(data)}`
+  ].decode(data);
+};
 
-export const fetchLmnCollections = async () => {
-  const data = {
-    searches: [
-      {
-        query_by: "collectionName,owner",
-        sort_by: "deployed:desc,fractionMinted:desc",
-        exclude_fields: "collectionBannerUrl",
-        highlight_full_fields: "collectionName,owner",
-        collection: "collections",
-        q: "*",
-        facet_by: "",
-        page: 1,
-      },
-    ],
-  };
+export const fetchLmnCollections = async (owner: PublicKey) => {
+  const searches = [
+    {
+      query_by: "owner",
+      sort_by: "deployed:desc,fractionMinted:desc",
+      exclude_fields: "collectionBannerUrl",
+      highlight_full_fields: "owner",
+      collection: "collections",
+      q: `${owner}`,
+      facet_by: "",
+      page: 1,
+    },
+  ];
 
   const res = await axios.post(
     "https://s.launchmynft.io/multi_search",
-    JSON.stringify(data),
+    JSON.stringify({ data: { searches } }),
     {
       params: {
         "x-typesense-api-key":
@@ -67,49 +69,20 @@ export const fetchLmnCollections = async () => {
 };
 
 /* Private helpers */
-
-type CandyMachine = CandyMachineV5 | CandyMachineV6;
-
-type CandyMachineType = typeof CandyMachineV5 | typeof CandyMachineV6;
-
-type CandyMachineVersion = `v${5 | 6}`;
-
-type CandyMachineInfo = {
-  version: CandyMachineVersion;
-  machine: CandyMachineType;
-};
-
-const INFO_LOOKUP: Record<string, CandyMachineInfo> = {
-  [CandyMachineV5.discriminator.toString()]: {
-    version: "v5",
-    machine: CandyMachineV5,
-  },
-  [CandyMachineV6.discriminator.toString()]: {
-    version: "v6",
-    machine: CandyMachineV6,
-  },
-};
-
-const lookup = (data: Buffer): CandyMachineInfo => {
+const getCandyMachineVersion = (data: Buffer): CandyMachineVersion => {
   const disc = data.subarray(0, 8).toString();
-  if (!INFO_LOOKUP[disc])
-    throw new Error("Invalid account or CandyMachine version not supported.");
-  return INFO_LOOKUP[disc];
+  switch (disc) {
+    case CandyMachineV2.discriminator.toString():
+      return "V2";
+    case CandyMachineV3.discriminator.toString():
+      return "V3";
+    case CandyMachineV4.discriminator.toString():
+      return "V4";
+    case CandyMachineV5.discriminator.toString():
+      return "V5";
+    case CandyMachineV6.discriminator.toString():
+      return "V6";
+    default:
+      throw new Error("Invalid account or CandyMachine version not supported.");
+  }
 };
-
-const fetchAccounts = (
-  connection: Connection,
-  filters: GetProgramAccountsFilter[]
-) => {
-  return connection.getProgramAccounts(PROGRAM_ID, {
-    filters,
-    encoding: "base64",
-  });
-};
-
-const accountFilter = (discriminator: Buffer) =>
-  memcmp(0, bs58.encode(discriminator));
-
-const memcmp = (offset: number, bytes: string): MemcmpFilter => ({
-  memcmp: { bytes, offset },
-});
