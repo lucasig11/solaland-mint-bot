1da58c7c722676c3bd3ad10b1c611a47b79e8935
lucas <lucasfonsecard@icloud.com>
2022-11-28 21:40:25 -0300

    feat: create default config file if it does not exist

--- launch-my-nft/bot/config.ts
+++ launch-my-nft/bot/config.ts
@@ -15,7 +15,31 @@ export interface Config {
 }
 
 export const readConfigFile = (file: string): Config => {
-  const contents = fs.readFileSync(path.resolve(process.cwd(), file), "utf8");
+  const p = path.resolve(process.cwd(), file);
+
+  if (!fs.existsSync(p)) {
+    console.log(`Config file ${file} not found!`);
+    console.log("Creating a new file with placeholder values...");
+
+    const defaultConfig: Config = {
+      rpcUrl: "https://api.mainnet-beta.solana.com",
+      interval: 1000,
+      tasks: [
+        {
+          maxMintAmount: 1,
+          startDate: new Date(),
+          payerKeypairFile: "payer.json",
+          hyperspaceUrl: "https://hyperspace.solana.com/candy/1",
+        },
+      ],
+    };
+
+    fs.writeFileSync(p, JSON.stringify(defaultConfig, null, 2));
+    console.log(`Config file created ${file}! Please edit it and try again.`);
+    process.exit(1);
+  }
+
+  const contents = fs.readFileSync(p, "utf8");
   const parsed = JSON.parse(contents, (k, v) => {
     switch (k) {
       case "tasks":
